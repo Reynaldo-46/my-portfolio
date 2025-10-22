9968490d7b80a1bd7568b178d5ba293b3d29c995
Reynaldo Yasoña <reynaldo.yasona06@gmail.com>
2025-10-22 15:05:27 +0800

    update portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/my-portfolio/', // <-- VERY IMPORTANT FOR GITHUB PAGES
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
